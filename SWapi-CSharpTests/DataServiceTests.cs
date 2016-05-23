431b1df562686748039e6d7c6a6bdab9fdd376f4
M-Yankov <pointblank12345@abv.bg>
2016-05-23 22:49:29 +0300

    Added Parser and Helper tests

---
+++ SWapi-CSharpTests/DataServiceTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace SWapi_CSharpTests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using StarWarsApiCSharp;
+    using Moq;
+    using System.Net;
+    using System.Web;
+    using System.Runtime.Serialization;
+    using System.Linq;
+
+    [TestClass]
+    public class DataServiceTests
+    {
+        [TestMethod]
+        public void ExpectToReturnCorrectResultsWhenResponseIsOk()
+        {
+            var mock = new Mock<IWebHelper>();
+
+            string expectedResult = string.Join(" ", Enumerable.Repeat("test", 100));
+            mock.Setup(w => w.GetResponse(It.IsAny<WebRequest>()))
+                .Returns(new TestWebResponse(expectedResult));
+
+            var res = new DefaultDataService(mock.Object)
+                .GetDataResult("http://testUrl.com");
+
+            Assert.IsNotNull(res);
+            Assert.AreEqual(expectedResult, res);
+        }
+
+        [TestMethod]
+        public void ExpectToReturnNullWhenResponseHasAnException()
+        {
+            var mock = new Mock<IWebHelper>();
+
+            mock.Setup(w => w.GetResponse(It.IsAny<WebRequest>()))
+                .Throws(new WebException());
+
+            var res = new DefaultDataService(mock.Object)
+                .GetDataResult("http://testUrl.com");
+
+            Assert.IsNull(res);
+        }
+    }
+}
---
+++ SWapi-CSharpTests/HelperTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace SWapi_CSharpTests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Net;
+    using Moq;
+    using StarWarsApiCSharp;
+    using System.IO;
+
+    [TestClass]
+    public class HelperTests
+    {
+        [TestMethod]
+        public void ExpectGettingRequestObjectToWorkCorrect()
+        {
+            const string validUrl = "http://testsite.com/";
+            WebRequest request = new WebHelper().GetRequest(validUrl);
+            Assert.AreEqual(validUrl, request.RequestUri.ToString());
+        }
+
+        [TestMethod]
+        public void ExpectRetreiveResponseFromRequestReturnsCorrectValue()
+        {
+            var mock = new Mock<WebRequest>();
+
+            const string ExpectedReturnValue = "Testing";
+            mock.Setup(r => r.GetResponse())
+                .Returns(new TestWebResponse(ExpectedReturnValue));
+
+            WebResponse response = new WebHelper().GetResponse(mock.Object);
+            StreamReader stream = new StreamReader(response.GetResponseStream());
+            var actial = stream.ReadToEnd();
+
+            stream.Dispose();
+
+            Assert.AreEqual(ExpectedReturnValue, actial);
+        }
+    }
+}
